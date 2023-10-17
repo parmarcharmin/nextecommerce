11208af1c94c17c9979cb2853ec706dfcb9b5b95
parmarcharmin <109529140+parmarcharmin@users.noreply.github.com>
2023-10-17 22:45:04 +0530

    Add files via upload

---
+++ public/products.js
@@ -0,0 +1,117 @@
+export const products = [
+   
+    {
+      "productRating": 0,
+      "_id": "64d29494c7fa1d36d8b51784",
+      "UserId": "64d28b474f9e44d485e045a4",
+      "productCategory": "men",
+      "productCategoryID": "64d28b474f9e44d485e045a4",
+      "productName": "ElvireCloths",
+      "productDescription": "cloths are printed with various designs ",
+      "productPrice": 500,
+      "productImage": [
+        "https://elvire-clothing.myshopify.com/cdn/shop/files/06_e00425b3-77b9-4014-806d-d50174ac891d.jpg?v=1689931789&width=823"
+      ],
+      "__v": 0,
+      "productType": "shirt"
+    },
+    {
+      "productRating": 0,
+      "_id": "64d354717f42e2787ede364c",
+      "UserId": "64d28b474f9e44d485e045a4",
+      "productCategory": "men",
+      "productCategoryID": "64d28b474f9e44d485e045a4",
+      "productName": "Amazon Branded",
+      "productDescription": "cloths are well manufactured with various designs ",
+      "productPrice": 500,
+      "productImage": [
+        "https://elvire-clothing.myshopify.com/cdn/shop/files/06_e00425b3-77b9-4014-806d-d50174ac891d.jpg?v=1689931789&width=823"
+      ],
+      "__v": 0,
+      "productType": "shirt"
+    },
+    {
+      "productRating": 0,
+      "_id": "64d35881fc362358ea16be61",
+      "UserId": "64d28b474f9e44d485e045a4",
+      "productCategory": "men",
+      "productCategoryID": "64d28b474f9e44d485e045a4",
+      "productName": "Violet Dreams: The Perfect Purple Flower Shirt",
+      "productDescription": "Violet Dreams: The Perfect Purple Flower Shirt\" is an exquisite and enchanting garment that will undoubtedly captivate your imagination.",
+      "productPrice": 700,
+      "productImage": [
+        "https://elvire-clothing.myshopify.com/cdn/shop/files/03.jpg?v=1689858934&width=823"
+      ],
+      "__v": 0
+    },
+    {
+      "_id": "64dc9fbb84732587db03b679",
+      "productRating": 0,
+      "productImage": [
+        "Pattern Is Printed. Style Code Is Df-sk_594. Type Is Fit And Flare. Pack Of Is 1. Sleeve Length Is 3/4 Sleeve. Length Is Above Knee/mid Thigh Length."
+      ],
+      "productCategoryID": "64d28b474f9e44d485e045a3",
+      "productName": " Demotex Fashion Women Fit And Flare Pink, White Dress",
+      "productDescription": "Dimensions of Big Table: 46.5cm (Height) x 53cm (Width). Dimensions of Small Table: 41cm (Height) x 40cm (Width).",
+      "productPrice": 600,
+      "__v": 0,
+      "productCategory": "women"
+    },
+    {
+      "_id": "64dc9ff684732587db03b67b",
+      "productRating": 0,
+      "productImage": [
+        "https://rukminim2.flixcart.com/image/832/832/kkh6zrk0/dress/c/h/j/xl-shoulder-frill-dress-peach-moshe-original-imafzt94vx8vgzez.jpeg?q=70"
+      ],
+      "productCategoryID": "64d28b474f9e44d485e045a3",
+      "productName": "Alle Moda Women Fit and Flare Pink Dress",
+      "productDescription": "Pattern Is Solid. Style Code Is Suk-shoulder-frill-dress-peach. Pack Of Is 1. Sleeve Length Is Full Sleeve. Color Is Pink. Ideal For Is Women. ",
+      "productPrice": 700,
+      "__v": 0,
+      "productCategory": "women"
+    },
+    {
+      "_id": "64dca03c84732587db03b67d",
+      "productRating": 0,
+      "productImage": [
+        "https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/9/i/f/l-women-printed-viscose-rayon-straight-kurta-kpf-original-imagqjezgm2ubu4c.jpeg?q=70"
+      ],
+      "productCategoryID": "64d28b474f9e44d485e045a3",
+      "productName": "KPF Women Printed Viscose Rayon A-line Kurta (Multicolor) ",
+      "productDescription": "Pattern Is leaves. Style Code Is Suk-shoulder-frill-dress-peach. Pack Of Is 1. Sleeve Length Is Full Sleeve. Color Is Pink. Ideal For Is Women. ",
+      "productPrice": 700,
+      "__v": 0,
+      "productCategory": "women"
+    },
+    {
+      "_id": "64dca06984732587db03b67f",
+      "productRating": 0,
+      "productImage": [
+        "https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/9/i/f/l-women-printed-viscose-rayon-straight-kurta-kpf-original-imagqjezgm2ubu4c.jpeg?q=70"
+      ],
+      "productCategoryID": "64d28b474f9e44d485e045a3",
+      "productName": " Funday Fashion Women Fit And Flare Light Blue Dress",
+      "productDescription": "Pattern Is Printed. Style Code Is Sys-9115. Type Is Fit And Flare. Length Is Above Knee/mid Thigh Length. Color Is Light Blue. ",
+      "productPrice": 600,
+      "__v": 0,
+      "productCategory": "women"
+    },
+    {
+      "_id": "64dcc3c09b285c1b49606e07",
+      "productRating": 0,
+      "productImage": [
+        "https://rukminim2.flixcart.com/image/832/832/xif0q/kurta/9/i/f/l-women-printed-viscose-rayon-straight-kurta-kpf-original-imagqjezgm2ubu4c.jpeg?q=70",
+        "https://www.fabfunda.com/product-img/Galaxi-Cotton-kurti-1608965039.jpeg",
+        "https://ahika.in/cdn/shop/articles/1_1296x_91a97c0e-35db-4bc9-8fd6-5948ed7436f2.jpg?v=1649144160",
+        "https://peachmode.com/cdn/shop/products/red-casual-wear-floral-printed-heavy-silk-long-kurti-peachmode-1.jpg?v=1669020655",
+        "https://peachmode.com/cdn/shop/products/red-casual-wear-floral-printed-heavy-silk-long-kurti-peachmode-2.jpg?v=1669020657"
+      ],
+      "productCategoryID": "64d28b474f9e44d485e045a3",
+      "productName": " Fundah Fashion Women Fit And Flare Light Blue Dress",
+      "productDescription": "Pattern Is Printed. Style Code Is Sys-9115. Type Is Fit And Flare. Length Is Above Knee/mid Thigh Length. Color Is Light Blue.",
+      "productPrice": 800,
+      "productSlug": "fundah-fashion-women-fit-and-flare-light-blue-dress",
+      "__v": 0,
+      "productCategory": "women"
+    }
+  ];
